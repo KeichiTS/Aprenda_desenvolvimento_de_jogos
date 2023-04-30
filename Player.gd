6557c647497d9d8384d388060a7aad34c9b56905
unknown <kkeichi@gmail.com>
2023-04-29 23:32:55 -0300

    [200~21. Variáveis do Tipo Array | Listas.~

--- Player.gd
+++ Player.gd
@@ -1,8 +1,13 @@
 extends Node2D
 class_name player
 
-var lista_de_numeros: int = 150
+var lista_de_produtos: Array = [
+	['Maçã', 10, 0.50],
+	['Banana', 12, 0.25],
+	['Laranja', 6, 0.35]
+]
 
 func _ready():
-	for numero in lista_de_numeros:
-		print(numero + 1)
+	for produto in lista_de_produtos:
+		for informacao in produto:
+			print(informacao)
