6280ef708d9d7205c1935910aabd50e58acf20d4
unknown <kkeichi@gmail.com>
2023-04-29 23:11:25 -0300

    17

--- Player.gd
+++ Player.gd
@@ -5,8 +5,16 @@ var enemy_health: int = 5
 
 var bonus_attack: int = 2 
 var base_attack: int = 3
+
+var can_attack: bool = true
 	
 func _ready():
+	if can_attack == true:
+		update_enemy_health()
+	else: 
+		print('Personagem impossibilitado de atacar')
+
+func update_enemy_health() -> void:
 	if base_attack + bonus_attack  >= enemy_health:
 		print('Matou o inimigo.')
 	elif base_attack + bonus_attack >= 3:
