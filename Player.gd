ec692e196978b6b229de483e451b6eb596d7dc9a
unknown <kkeichi@gmail.com>
2023-04-29 23:25:46 -0300

    20. Estrutura de Repetição For.~

--- Player.gd
+++ Player.gd
@@ -1,23 +1,8 @@
 extends Node2D
 class_name player
 
+var lista_de_numeros: int = 150
 
-
-var bonus_attack: int = 2 
-var base_attack: int = 0
-
-	
 func _ready():
-	match base_attack + bonus_attack:
-		0:
-			print('inimigo tomou 0 de dano')
-		1:
-			print('inimigo tomou 1 de dano')
-		2:
-			print('inimigo tomou 2 de dano')
-		3:
-			print('inimigo tomou 3 de dano')
-		4:
-			print('inimigo tomou 4 de dano')
-		_:
-			print('dano desconhecido')
+	for numero in lista_de_numeros:
+		print(numero + 1)
