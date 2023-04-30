462cdf392a34f3ca7db4df679e83eee3910953ae
unknown <kkeichi@gmail.com>
2023-04-29 23:22:46 -0300

    19. Condicional Match.

--- Player.gd
+++ Player.gd
@@ -1,24 +1,23 @@
 extends Node2D
 class_name player
 
-var enemy_health: int = 5
+
 
 var bonus_attack: int = 2 
-var base_attack: int = 3
+var base_attack: int = 0
 
-var can_attack: bool = false
 	
 func _ready():
-	print(update_enemy_health())
-
-
-func update_enemy_health() -> String:
-	if can_attack == true:
-		if base_attack + bonus_attack  >= enemy_health:
-			return 'Matou o inimigo.'
-		elif base_attack + bonus_attack >= 3:
-			return 'Inimigo tomou 60% da vida em dano.'
-		else:
-			return 'Inimigo sobreviveu ao ataque'
-	else: 
-		return 'Personagem impossibilitado de atacar'
+	match base_attack + bonus_attack:
+		0:
+			print('inimigo tomou 0 de dano')
+		1:
+			print('inimigo tomou 1 de dano')
+		2:
+			print('inimigo tomou 2 de dano')
+		3:
+			print('inimigo tomou 3 de dano')
+		4:
+			print('inimigo tomou 4 de dano')
+		_:
+			print('dano desconhecido')
