c0cafcb7cc36dc2ef8c16ec07de3681370e889f6
KeichiTS <kkeichi@gmail.com>
2023-05-03 21:00:51 -0300

    27. Script do ParallaxBackground.

---
+++ scripts/env/background.gd
@@ -0,0 +1,16 @@
+extends ParallaxBackground
+class_name Background
+
+@export var can_process: bool
+@export var layer_speed : Array[int]
+
+func _ready():
+	if can_process == false:
+		set_physics_process(false)
+		
+
+func _physics_process(delta):
+	for index in get_child_count():
+		if get_child(index) is ParallaxLayer:
+			get_child(index).motion_offset.x -= delta * layer_speed[index]
+			
