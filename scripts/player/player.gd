a252fd8b393558f709de34a4dd47c8a1a6a5c02a
KeichiTS <kkeichi@gmail.com>
2023-11-20 08:49:58 -0300

    player_code_move_and_slide_in_x

---
+++ scripts/player/player.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+class_name Player
+
+@onready var player_sprite: Sprite2D = get_node('Texture')
+@export var speed : int 
+
+func _physics_process(delta: float):
+	horizontal_movement_env()
+	move_and_slide()
+
+func horizontal_movement_env() -> void:
+	var input_direction : float = Input.get_action_strength('ui_right') - Input.get_action_strength('ui_left')
+	velocity.x = input_direction * speed
