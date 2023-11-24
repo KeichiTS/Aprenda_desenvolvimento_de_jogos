adcb29e5816b42e5502791224660afcf067bcce6
KeichiTS <kkeichi@gmail.com>
2023-11-23 23:50:44 -0300

    wall_jump_speed

--- scripts/player/player_texture.gd
+++ scripts/player/player_texture.gd
@@ -14,7 +14,7 @@ var crouching_off: bool = false
 func animate(direction: Vector2) -> void:
 	verify_position(direction)
 	
-	if player.attacking or player.defending or player.crouching:
+	if player.attacking or player.defending or player.crouching or player.next_to_wall():
 		action_behavior()
 	
 	elif direction.y != 0: 
@@ -29,12 +29,19 @@ func verify_position(direction : Vector2) -> void:
 	if direction.x > 0:
 		flip_h = false
 		suffix = '_right'
+		player.direction = -1
+		position = Vector2.ZERO
+		player.wall_ray.target_position = Vector2(10.5,0)
 	elif direction.x < 0: 
 		suffix = '_left'
+		player.direction = 1
 		flip_h = true
-
+		position = Vector2( -2, 0 )
+		player.wall_ray.target_position = Vector2(-12.5,0)
 func action_behavior() -> void:
-	if player.attacking and normal_attack:
+	if player.next_to_wall():
+		animation.play('wall_slide')
+	elif player.attacking and normal_attack:
 		animation.play('attack' + suffix)
 	elif player.defending and shield_off:
 		animation.play('shield')
