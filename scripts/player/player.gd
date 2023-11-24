cd0db50dc9fe068aa31c358cee8d8f68be3f57aa
KeichiTS <kkeichi@gmail.com>
2023-11-23 23:38:43 -0300

    wall_jump_gravity_and_codes

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -2,18 +2,27 @@ extends CharacterBody2D
 class_name Player
 
 @onready var player_sprite: Sprite2D = get_node('Texture')
+@onready var wall_ray : RayCast2D = get_node('WallRay')
 
+var direction: int = 1 
 var jump_count : int = 0
+
 var landing: bool = false 
+var on_wall: bool = false 
 var attacking: bool = false 
 var defending: bool = false 
 var crouching: bool = false 
 
+var not_on_wall: bool = true
 var can_track_input: bool = true
 
 @export var speed : int 
 @export var jump_speed : int
+@export var wall_jump_speed: int
+
+@export var wall_gravity : int 
 @export var player_gravity : int
+@export var wall_impulse_speed : int
 
 func _physics_process(delta: float):
 	horizontal_movement_env()
@@ -33,13 +42,17 @@ func horizontal_movement_env() -> void:
 	velocity.x = input_direction * speed
 
 func vertival_movement_env() -> void:
-	if is_on_floor():
+	if is_on_floor() or is_on_wall():
 		jump_count = 0
 		
 	var jump_condition : bool = can_track_input and not attacking
 	if Input.is_action_just_pressed('ui_select') and jump_count < 2 and jump_condition:
 		jump_count += 1 
-		velocity.y = jump_speed
+		if next_to_wall() and not is_on_floor():
+			velocity.y = wall_jump_speed
+			velocity.x += wall_impulse_speed * direction
+		else:
+			velocity.y = jump_speed
 
 func actions_env() -> void:
 	attack()
@@ -47,10 +60,25 @@ func actions_env() -> void:
 	defense()
 
 func gravity(delta : float) -> void:
-	velocity.y += delta * player_gravity
-	if velocity.y >= player_gravity:
-		velocity.y = player_gravity
+	if next_to_wall():
+		velocity.y += wall_gravity * delta
+		if velocity.y >= wall_gravity:
+			velocity.y = wall_gravity
+	else:
+		velocity.y += delta * player_gravity
+		if velocity.y >= player_gravity:
+			velocity.y = player_gravity
 
+func next_to_wall() -> bool:
+	if wall_ray.is_colliding() and not is_on_floor():
+		if not_on_wall:
+			velocity.y = 0 
+			not_on_wall = false
+		return true
+	
+	else:
+		not_on_wall = true
+		return false
 func attack() -> void:
 	var attack_condition: bool = not attacking and not crouching and not defending
 	if Input.is_action_just_pressed('attack') and attack_condition and is_on_floor():
