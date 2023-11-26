6dbe65e6d2004398c35f316b3828dedaf85c2d36
KeichiTS <kkeichi@gmail.com>
2023-11-25 23:28:30 -0300

    enemy_template_code

---
+++ scripts/enemy/detection_area.gd
@@ -0,0 +1,14 @@
+extends Area2D
+class_name DetectionArea
+
+@export var enemy_path : NodePath
+@onready var enemy : CharacterBody2D = get_node(enemy_path)
+
+func on_body_entered(body: Player) -> void :
+	enemy.player_ref = body
+
+
+
+func on_body_exited(_body: Player) -> void:
+	enemy.player_ref = null
+	pass # Replace with function body.
