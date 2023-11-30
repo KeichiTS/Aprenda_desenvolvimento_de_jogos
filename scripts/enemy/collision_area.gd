a5824d0e822cd643f44651554b8c8dc07d07902e
KeichiTS <kkeichi@gmail.com>
2023-11-29 22:08:06 -0300

    enemy_collision_area_scripts

---
+++ scripts/enemy/collision_area.gd
@@ -0,0 +1,22 @@
+extends Area2D
+class_name CollisionArea
+
+@onready var timer: Timer = get_node('Timer')
+
+@export var health : int
+@export var invulnerability_timer : float
+@export var enemy : CharacterBody2D
+
+func on_collision_area_entered(area):
+	if area.get_parent() is Player:
+		var player_stats : Node = area.get_parent().get_node('Stats')
+		var player_attack : int = player_stats.base_attack + player_stats.bonus_attack
+		update_health(player_attack)
+		
+func update_health(damage : int) -> void:
+	health -= damage
+	if health <= 0:
+		enemy.can_day = true 
+		return
+	
+	enemy.can_hit = true 
