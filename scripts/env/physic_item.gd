8d65c27f2e1bebc49cc872e9de0814a396156e5a
KeichiTS <kkeichi@gmail.com>
2023-12-05 23:36:05 -0300

    collect_item_effect

--- scripts/env/physic_item.gd
+++ scripts/env/physic_item.gd
@@ -1,6 +1,8 @@
 extends RigidBody2D
 class_name PhysicItem
 
+const COLLECT_EFFECT : PackedScene = preload('res://scenes/effect/general_effects/collect_item.tscn')
+
 @onready var sprite : Sprite2D = get_node('Texture')
 
 var player_ref : CharacterBody2D = null
@@ -46,4 +48,11 @@ func on_body_exited(_body : Player):
 func _process(delta: float) -> void:
 	if player_ref != null and Input.is_action_just_pressed('interact'):
 		#emitir sinal para enviar item para o inventario 
+		spawn_effect()
 		queue_free()
+
+func spawn_effect() -> void:
+	var collect_effect : EffectTemplate = COLLECT_EFFECT.instantiate()
+	get_tree().root.call_deferred('add_child', collect_effect)
+	collect_effect.global_position = global_position
+	collect_effect.play_effect()
